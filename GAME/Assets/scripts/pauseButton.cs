767a204933b46e9e04fffe4d10546b71acfca5a7
guptabhoomika <bhoomika.189301123@muj.manipal.edu>
2019-07-18 11:41:05 +0530

    Slight modifications

---
+++ GAME/Assets/scripts/pauseButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+using UnityEngine.UI;
+ public class pauseButton : MonoBehaviour
+{
+    public GameObject pauseUI;
+    public fader screenFader;
+   
+    
+    public void Toggle()
+    {
+
+        pauseUI.SetActive(!pauseUI.activeSelf);
+        if (pauseUI.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+}
